ba8c7f35c058fe23b5e880173a158aedaf6a2fce
Mikhail Krutov <mkrutov@redhat.com>
2020-01-30 16:21:04 +0100

    Fix autogenerated method to avoid calls to non-existent packages

--- pkg/apis/addtoscheme_broker_v2alpha1.go
+++ pkg/apis/addtoscheme_broker_v2alpha1.go
@@ -1,11 +1,10 @@
 package apis
 
 import (
-	routev1 "github.com/openshift/api/route/v1"
 	"github.com/rh-messaging/activemq-artemis-operator/pkg/apis/broker/v2alpha1"
 )
 
 func init() {
 	// Register the types with the Scheme so the components can map objects to GroupVersionKinds and back
-	AddToSchemes = append(AddToSchemes, v2alpha1.SchemeBuilder.AddToScheme, routev1.SchemeBuilder.AddToScheme)
+	AddToSchemes = append(AddToSchemes, v2alpha1.SchemeBuilder.AddToScheme, v2alpha1.SchemeBuilder.AddToScheme)
 }
