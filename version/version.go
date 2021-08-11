8cbdb8b82daa149183953847f2aeadac81e48c48
Howard Gao <howard_gao@gmail.com>
2021-08-11 12:01:49 +0800

    Issue #67 Adding extra mounts for user specified secrets and configmaps ENTMQBR-4637 Provide optional secret/cofigmap in the Custom Resource

--- version/version.go
+++ version/version.go
@@ -1,17 +1,17 @@
 package version
 
 var (
-	Version = "7.8.2"
+	Version = "7.9.0"
 	// PriorVersion - prior version
-	PriorVersion = "7.8.1"
+	PriorVersion = "7.8.2"
 )
 
 const (
 	// LatestVersion product version supported
-	LatestVersion        = "7.8.2"
-	CompactLatestVersion = "782"
+	LatestVersion        = "7.9.0"
+	CompactLatestVersion = "790"
 	// LastMicroVersion product version supported
-	LastMicroVersion = "7.8.1"
+	LastMicroVersion = "7.8.2"
 	// LastMinorVersion product version supported
 	LastMinorVersion = "7.7.0"
 )
@@ -23,14 +23,16 @@ var OperandVersionFromOperatorVersion map[string]string = map[string]string{
 	"0.17.0": "7.7.0",
 	"0.18.0": "7.8.0",
 	"0.19.0": "7.8.1",
-	"7.8.1": "7.8.1",
-	"7.8.2": "7.8.2",
+	"7.8.1":  "7.8.1",
+	"7.8.2":  "7.8.2",
+	"7.9.0":  "7.9.0",
 }
 var FullVersionFromMinorVersion map[string]string = map[string]string{
 	"70": "7.7.0",
 	"80": "7.8.0",
 	"81": "7.8.1",
 	"82": "7.8.2",
+	"90": "7.9.0",
 }
 
 var CompactFullVersionFromMinorVersion map[string]string = map[string]string{
@@ -38,6 +40,7 @@ var CompactFullVersionFromMinorVersion map[string]string = map[string]string{
 	"80": "780",
 	"81": "781",
 	"82": "782",
+	"90": "790",
 }
 
 var CompactVersionFromVersion map[string]string = map[string]string{
@@ -45,6 +48,7 @@ var CompactVersionFromVersion map[string]string = map[string]string{
 	"7.8.0": "780",
 	"7.8.1": "781",
 	"7.8.2": "782",
+	"7.9.0": "790",
 }
 
 var FullVersionFromCompactVersion map[string]string = map[string]string{
@@ -52,6 +56,7 @@ var FullVersionFromCompactVersion map[string]string = map[string]string{
 	"780": "7.8.0",
 	"781": "7.8.1",
 	"782": "7.8.2",
+	"790": "7.9.0",
 }
 
 var MinorVersionFromFullVersion map[string]string = map[string]string{
@@ -59,6 +64,7 @@ var MinorVersionFromFullVersion map[string]string = map[string]string{
 	"7.8.0": "80",
 	"7.8.1": "81",
 	"7.8.2": "82",
+	"7.9.0": "90",
 }
 
 var YacfgProfileName string = "amq_broker"
