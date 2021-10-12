551dd48ecf22639635aa8a7b3d386fa7fa6d884a
Howard Gao <howard.gao@gmail.com>
2021-10-12 23:46:21 +0800

    Update version.go to add 7.8.3

--- version/version.go
+++ version/version.go
@@ -1,17 +1,17 @@
 package version
 
 var (
-	Version = "7.8.2"
+	Version = "7.8.3"
 	// PriorVersion - prior version
-	PriorVersion = "7.8.1"
+	PriorVersion = "7.8.2"
 )
 
 const (
 	// LatestVersion product version supported
-	LatestVersion        = "7.8.2"
-	CompactLatestVersion = "782"
+	LatestVersion        = "7.8.3"
+	CompactLatestVersion = "783"
 	// LastMicroVersion product version supported
-	LastMicroVersion = "7.8.1"
+	LastMicroVersion = "7.8.2"
 	// LastMinorVersion product version supported
 	LastMinorVersion = "7.7.0"
 )
@@ -25,12 +25,14 @@ var OperandVersionFromOperatorVersion map[string]string = map[string]string{
 	"0.19.0": "7.8.1",
 	"7.8.1": "7.8.1",
 	"7.8.2": "7.8.2",
+	"7.8.3": "7.8.3",
 }
 var FullVersionFromMinorVersion map[string]string = map[string]string{
 	"70": "7.7.0",
 	"80": "7.8.0",
 	"81": "7.8.1",
 	"82": "7.8.2",
+	"83": "7.8.3",
 }
 
 var CompactFullVersionFromMinorVersion map[string]string = map[string]string{
@@ -38,6 +40,7 @@ var CompactFullVersionFromMinorVersion map[string]string = map[string]string{
 	"80": "780",
 	"81": "781",
 	"82": "782",
+	"83": "783",
 }
 
 var CompactVersionFromVersion map[string]string = map[string]string{
@@ -45,6 +48,7 @@ var CompactVersionFromVersion map[string]string = map[string]string{
 	"7.8.0": "780",
 	"7.8.1": "781",
 	"7.8.2": "782",
+	"7.8.3": "783",
 }
 
 var FullVersionFromCompactVersion map[string]string = map[string]string{
@@ -52,6 +56,7 @@ var FullVersionFromCompactVersion map[string]string = map[string]string{
 	"780": "7.8.0",
 	"781": "7.8.1",
 	"782": "7.8.2",
+	"783": "7.8.3",
 }
 
 var MinorVersionFromFullVersion map[string]string = map[string]string{
@@ -59,6 +64,7 @@ var MinorVersionFromFullVersion map[string]string = map[string]string{
 	"7.8.0": "80",
 	"7.8.1": "81",
 	"7.8.2": "82",
+	"7.8.3": "83",
 }
 
 var YacfgProfileName string = "amq_broker"
