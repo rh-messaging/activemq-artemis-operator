eeb122b0ff9d6a8bc174899b48e306d771e5e933
rh-messaging-ci <messaging-infra@redhat.com>
2023-07-25 11:50:24 +0000

    Upgrade AMQ Broker container images to 7.10.3.CON.1.CR2

--- pkg/resources/pods/additional_labels.go
+++ pkg/resources/pods/additional_labels.go
@@ -21,6 +21,7 @@ var productVerFromImageVer map[string]string = map[string]string{
 	"7.10.2": "2022.Q4",
 	"7.11.0": "2023.Q1",
 	"7.11.1": "2023.Q2",
+	"7.10.3": "2023.Q2",
 }
 
 // the labels returned will be added to broker pod
--- version/version.go
+++ version/version.go
@@ -48,6 +48,7 @@ var FullVersionFromCompactVersion map[string]string = map[string]string{
 	"7102": "7.10.2",
 	"7110": "7.11.0",
 	"7111": "7.11.1",
+	"7103": "7.10.3",
 }
 
 //The yacfg profile to use for a given full version of broker
@@ -67,6 +68,7 @@ var YacfgProfileVersionFromFullVersion map[string]string = map[string]string{
 	"7.10.2": "7.10.0",
 	"7.11.0": "7.10.0",
 	"7.11.1": "7.10.0",
+	"7.10.3": "7.10.0",
 }
 
 var YacfgProfileName string = "amq_broker"
@@ -78,6 +80,7 @@ var SupportedActiveMQArtemisVersions = []string{
 	"7.10.2",
 	"7.11.0",
 	"7.11.1",
+	"7.10.3",
 }
 
 func CompactActiveMQArtemisVersion(version string) string {
