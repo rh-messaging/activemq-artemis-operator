dc80f7e59249283d2c8529c5f181fd7f3742ee1b
rh-messaging-ci <messaging-infra@redhat.com>
2023-10-13 07:09:08 +0000

    Upgrade AMQ Broker 7.10.4 container images

--- version/version.go
+++ version/version.go
@@ -10,15 +10,15 @@ var (
 
 const (
 	// LatestVersion product version supported
-	LatestVersion        = "7.10.3"
-	CompactLatestVersion = "7103"
+	LatestVersion        = "7.10.4"
+	CompactLatestVersion = "7104"
 	// LastMicroVersion product version supported
 	LastMicroVersion = "7.9.4"
 	// LastMinorVersion product version supported
 	LastMinorVersion = "7.7.0"
 
-	LatestKubeImage = "registry.redhat.io/amq7/amq-broker-rhel8:7.10.3"
-	LatestInitImage = "registry.redhat.io/amq7/amq-broker-init-rhel8:7.10.3"
+	LatestKubeImage = "registry.redhat.io/amq7/amq-broker-rhel8:7.10.4"
+	LatestInitImage = "registry.redhat.io/amq7/amq-broker-init-rhel8:7.10.4"
 )
 
 func DefaultImageName(archSpecificRelatedImageEnvVarName string) string {
@@ -112,6 +112,7 @@ var FullVersionFromCompactVersion map[string]string = map[string]string{
 	"7101": "7.10.1",
 	"7102": "7.10.2",
 	"7103": "7.10.3",
+	"7104": "7.10.4",
 }
 
 var MinorVersionFromFullVersion map[string]string = map[string]string{
@@ -146,6 +147,7 @@ var YacfgProfileVersionFromFullVersion map[string]string = map[string]string{
 	"7.10.1": "7.10.0",
 	"7.10.2": "7.10.0",
 	"7.10.3": "7.10.0",
+	"7.10.4": "7.10.0",
 }
 
 var YacfgProfileName string = "amq_broker"
