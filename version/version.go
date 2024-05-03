5f71494e4ca58914a196d91c1bddf67d06714ea7
rh-messaging-ci <messaging-infra@redhat.com>
2024-05-03 08:50:47 +0000

    Upgrade AMQ Broker 7.12.0 container images

--- version/version.go
+++ version/version.go
@@ -75,7 +75,7 @@ var ActiveMQArtemisVersionfromFullVersion map[string]string = map[string]string{
 	"7.11.4": "2.28.0.redhat-00012",
 	"7.11.5": "2.28.0.redhat-00016",
 	"7.11.6": "2.28.0.redhat-00019",
-	"7.12.0": "2.33.0.redhat-00006",
+	"7.12.0": "2.33.0.redhat-00007",
 }
 
 func CompactActiveMQArtemisVersion(version string) string {
