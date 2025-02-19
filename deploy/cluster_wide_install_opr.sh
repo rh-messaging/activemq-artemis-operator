453e794b58c0717af397add3f209974f38891d7b
Domenico Francesco Bruscino <brusdev@apache.org>
2025-02-19 11:24:18 +0100

    [#1081] Fix cluster_wide_install_opr.sh on MacOS

--- deploy/cluster_wide_install_opr.sh
+++ deploy/cluster_wide_install_opr.sh
@@ -22,4 +22,6 @@ SERVICE_ACCOUNT_NS="$($KUBE_CLI get -f $DEPLOY_PATH/service_account.yaml -o json
 sed "s/namespace:.*/namespace: ${SERVICE_ACCOUNT_NS}/" $DEPLOY_PATH/cluster_role_binding.yaml | $KUBE_CLI apply -f -
 $KUBE_CLI create -f $DEPLOY_PATH/election_role.yaml
 $KUBE_CLI create -f $DEPLOY_PATH/election_role_binding.yaml
-sed -e "/WATCH_NAMESPACE/,/- name/ { /WATCH_NAMESPACE/b; /valueFrom:/bx; /- name/b; d; :x s/valueFrom:/value: '${WATCH_NAMESPACE}'/}" $DEPLOY_PATH/operator.yaml | $KUBE_CLI apply -f -
+cat $DEPLOY_PATH/operator.yaml | sed "/WATCH_NAMESPACE/,/metadata.namespace/ s/valueFrom:/value: '${WATCH_NAMESPACE}'/" |
+  sed "/WATCH_NAMESPACE/,/metadata.namespace/ s/fieldRef:/fieldref.namespace/" |
+  sed '/fieldref.namespace/,/metadata.namespace/d' | $KUBE_CLI apply -f -
