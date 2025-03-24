2502ed0e3473c010e2b05b1a0450735e5e5b4700
rh-messaging-ci <messaging-infra@redhat.com>
2025-03-24 14:13:40 +0000

    Upgrade AMQ Broker 7.12.3 container images

--- pkg/resources/pods/additional_labels.go
+++ pkg/resources/pods/additional_labels.go
@@ -16,6 +16,7 @@ var productVerFromImageVer map[string]string = map[string]string{
 	"7.12.0": "2024.Q2",
 	"7.12.1": "2024.Q3",
 	"7.12.2": "2024.Q3",
+	"7.12.3": "2025.Q1",
 }
 
 // the labels returned will be added to broker pod
