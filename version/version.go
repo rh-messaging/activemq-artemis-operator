a6fe5f153a07aaf27c5978c628099bb507ea7173
rh-messaging-ci <messaging-infra@redhat.com>
2025-06-15 00:04:46 +0000

    Upgrade AMQ Broker 8.0.0 container images

--- version/version.go
+++ version/version.go
@@ -96,7 +96,7 @@ var SupportedActiveMQArtemisVersions = []string{
 }
 
 var ActiveMQArtemisVersionfromFullVersion map[string]string = map[string]string{
-	"8.0.0": "2.42.0.temporary-redhat-00045",
+	"8.0.0": "2.42.0.temporary-redhat-00046",
 }
 
 func CompactActiveMQArtemisVersion(version string) string {
