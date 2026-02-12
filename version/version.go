3f717e92f1f690ca495b618feb1880def32002a8
rh-messaging-ci <messaging-infra@redhat.com>
2026-02-12 12:16:17 +0000

    Upgrade AMQ Broker 7.13.1 container images

--- version/version.go
+++ version/version.go
@@ -87,6 +87,7 @@ var FullVersionFromCompactVersion map[string]string = map[string]string{
 	"7124": "7.12.4",
 	"7125": "7.12.5",
 	"7130": "7.13.0",
+	"7131": "7.13.1",
 }
 
 // The yacfg profile to use for a given full version of broker
@@ -98,6 +99,7 @@ var YacfgProfileVersionFromFullVersion map[string]string = map[string]string{
 	"7.12.4": "7.10.0",
 	"7.12.5": "7.10.0",
 	"7.13.0": "7.10.0",
+	"7.13.1": "7.10.0",
 }
 
 var YacfgProfileName string = "amq_broker"
@@ -111,6 +113,7 @@ var SupportedActiveMQArtemisVersions = []string{
 	"7.12.4",
 	"7.12.5",
 	"7.13.0",
+	"7.13.1",
 }
 
 var ActiveMQArtemisVersionfromFullVersion map[string]string = map[string]string{
@@ -121,6 +124,7 @@ var ActiveMQArtemisVersionfromFullVersion map[string]string = map[string]string{
 	"7.12.4": "2.33.0.redhat-00017",
 	"7.12.5": "2.33.0.redhat-00018",
 	"7.13.0": "2.40.0.redhat-00004",
+	"7.13.1": "2.40.0.redhat-00006",
 }
 
 func CompactActiveMQArtemisVersion(version string) string {
